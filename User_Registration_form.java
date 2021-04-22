7f993b974512a3c433b24bc901662fbf2e429827
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-22 07:41:51 +0530

    Added:code as a user need to enter valid email Id

--- User_Registration_form.java
+++ User_Registration_form.java
@@ -8,7 +8,8 @@ class  User_Registration_form extends User
 	{
     Scanner sc=new Scanner(System.in); 
     User_Registration_form form=new User_Registration_form();
-    while(true){
+    
+	while(true){
       System.out.println("Enter First Name");
       String first_Name=sc.nextLine();
       if((form.isValid_firstName(first_Name))==true){
@@ -29,6 +30,18 @@ class  User_Registration_form extends User
       else
         System.out.println("Invalid...1st letter must be Capital");
     }
+
+	  while(true){
+      System.out.println("Enter Email Id");
+      String emailId=sc.nextLine();
+      if((form.isValid_emailId(emailId))==true){
+        System.out.println("Valid");
+        break;
+        }
+      else
+        System.out.println("Invalid.. enter valid email Id");
+    }
+
   }
 }
 
@@ -36,6 +49,7 @@ class User
 {
 	public String firstName;
 	public String lastName;
+	private String emailId;
 	Pattern p;
 	Matcher m;
 	String regex;
@@ -60,6 +74,18 @@ class User
           return false;
         }
 		m = p.matcher(lastName);
+		return m.matches();
+	}
+
+	protected boolean isValid_emailId(String emailId)
+	{
+    this.emailId=emailId;
+		regex="^[A-Z a-z 0-9]+([._+-][0-9 a-z A-Z]+)*![.]@([0-9A-Za-z]+).([A-Za-z]{2,3}).[A-Za-z]{0,2}$;
+		p = Pattern.compile(regex);
+		if (emailId == null) {
+          return false;
+        }
+		m = p.matcher(emailId);
 		return m.matches();
 	}
 
