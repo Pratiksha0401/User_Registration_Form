1ff5e0de018c1f03adaff22fb4a506bcf8f14023
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-22 07:15:17 +0530

    Added:code as a user need to enter valid last name

--- User_Registration_form.java
+++ User_Registration_form.java
@@ -1,27 +1,28 @@
 import java.util.regex.*;
 import java.util.*;
-class  User_Registration_form
+class  User_Registration_form extends User
 {
-	static private boolean isValid_firstName(User user)
-	{
-		String regex="^[A-Z]{1}[a-z]+";
-		Pattern p = Pattern.compile(regex);
-		if (user.firstName == null) {
-          return false;
-        }
-		Matcher m = p.matcher(user.firstName);
-		return m.matches();
-	}
+	
+
 	public static void main(String[] args) 
 	{
     Scanner sc=new Scanner(System.in); 
     User_Registration_form form=new User_Registration_form();
-    
     while(true){
       System.out.println("Enter First Name");
       String first_Name=sc.nextLine();
-      User user=new User(first_Name);
-      if((form.isValid_firstName(user))==true){
+      if((form.isValid_firstName(first_Name))==true){
+        System.out.println("Valid");
+        break;
+        }
+      else
+        System.out.println("Invalid...1st letter must be Capital");
+    }
+
+    while(true){
+      System.out.println("Enter last Name");
+      String last_Name=sc.nextLine();
+      if((form.isValid_lastName(last_Name))==true){
         System.out.println("Valid");
         break;
         }
@@ -34,13 +35,48 @@ class  User_Registration_form
 class User
 {
 	public String firstName;
+	public String lastName;
+	Pattern p;
+	Matcher m;
+	String regex;
+  protected boolean isValid_firstName(String first_Name)
+	{
+    this.firstName=first_Name;
+		regex="^[A-Z]{1}[a-z]+";
+		p = Pattern.compile(regex);
+		if (firstName == null) {
+          return false;
+        }
+		 m= p.matcher(firstName);
+		return m.matches();
+	}
 
-  public User(String first_Name){
-        this.firstName=first_Name;
-  }
+  protected boolean isValid_lastName(String last_Name)
+	{
+    this.lastName=last_Name;
+		regex="^[A-Z]{1}[a-z]+";
+		p = Pattern.compile(regex);
+		if (lastName == null) {
+          return false;
+        }
+		m = p.matcher(lastName);
+		return m.matches();
+	}
 
 	public String getfirstName()
   {
     return firstName;
   }
+  public void lastName(String firstname) {
+        this.firstName=firstname;
+  }
+
+  public String getlastName()
+  {
+    return firstName;
+  }
+  public void setlastName(String lastName) {
+        this.lastName=lastName;
+      }
+
 }
