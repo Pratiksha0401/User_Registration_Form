5305dc5e194cf933eea8b8d567ae26748155ca60
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-23 08:14:33 +0530

    Added : code to have At least 1 digit in password

--- User_Registration_form.java
+++ User_Registration_form.java
@@ -1,59 +1,111 @@
 import java.util.regex.*;
 import java.util.*;
-class  User_Registration_form extends User
+class  User_Registration_form
 {
-	
+	Pattern p;
+	Matcher m;
+	String regex;
+
+	protected boolean isValid_firstName(User user){
+		regex="^[A-Z]{1}[a-z]+";
+		p = Pattern.compile(regex);
+		if (user.getFirstName() == null) {
+          return false;
+        }
+		 m= p.matcher(user.getFirstName());
+		return m.matches();
+	}
+
+  protected boolean isValid_lastName(User user){
+		regex="^[A-Z]{1}[a-z]+";
+		p = Pattern.compile(regex);
+		if (user.getLastName()== null) {
+          return false;
+        }
+		m = p.matcher(user.getLastName());
+		return m.matches();
+	}
+
+	protected boolean isValid_emailId(User user){
+		regex="^[A-Z a-z 0-9]+([._+-][0-9 a-z A-Z]+)*@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}([.][a-z A-Z]{2})*$";
+		p = Pattern.compile(regex);
+		if (user.getEmailId() == null) {
+          return false;
+        }
+		m = p.matcher(user.getEmailId());
+		return m.matches();
+	}
+
+	protected boolean isValid_MobileNumber(User user){
+		regex="^[1-9]{2}\\s{0,1}[1-9]{1}[0-9]{9}$";
+		p = Pattern.compile(regex);
+		if (user.getMobileNumber()== null) {
+          return false;
+        }
+		m = p.matcher(user.getMobileNumber());
+		return m.matches();
+	}
+
+	protected boolean isValid_Password(User user){
+		regex="^([a-zA-Z0-9]{8,})$";
+		p = Pattern.compile(regex);
+		if (user.getPassword()== null) {
+          return false;
+        }
+		m = p.matcher(user.getPassword());
+		return m.matches();
+	}
 
 	public static void main(String[] args) 
 	{
     Scanner sc=new Scanner(System.in); 
     User_Registration_form form=new User_Registration_form();
-    
+    User user=new User();
+	
 	while(true){
       System.out.println("Enter First Name");
-      String first_Name=sc.nextLine();
-      if((form.isValid_firstName(first_Name))==true)
+      user.setFirstName(sc.nextLine());
+      if((form.isValid_firstName(user))==true)
         break;
       else
         System.out.println("Invalid...1st letter must be Capital");
-    }
+	  }
 
     while(true){
       System.out.println("Enter last Name");
-      String last_Name=sc.nextLine();
-      if((form.isValid_lastName(last_Name))==true)      
+      user.setLastName(sc.nextLine());
+      if((form.isValid_lastName(user))==true)   
         break;
       else
         System.out.println("Invalid...1st letter must be Capital");
-    }
+	 }
 
 	  while(true){
       System.out.println("Enter Email Id");
-      String emailId=sc.nextLine();
-      if((form.isValid_emailId(emailId))==true)
+      user.setEmailId(sc.nextLine());
+      if((form.isValid_emailId(user))==true)
         break;
       else
         System.out.println("Invalid.. enter valid email Id");
-    }
+	}
 
-	  while(true){
-      System.out.println("Enter Phone Number");
-      String phoneNumber=sc.nextLine();
-      if((form.isValid_phoneNumber(phoneNumber))==true)
+	while(true){
+      System.out.println("Enter Mobile Number");
+      user.setMobileNumber(sc.nextLine());
+      if((form.isValid_MobileNumber(user))==true)
         break;
       else
-        System.out.println("Invalid.. enter phone number");
-    }
+        System.out.println("Invalid.. enter 10 digit mobile number with 2 digit code");
+	}
 
-	 while(true){
-      System.out.println("Enter password");
-      String password=sc.nextLine();
-      if((form.isValid_password(password))==true)
+	while(true){
+      System.out.println("Enter Password");
+      user.setPassword(sc.nextLine());
+      if((form.isValid_Password(user))==true)
         break;
       else
-        System.out.println("Invalid.. enter at least 8 char password");
-    }
-
+        System.out.println("Invalid.. enter 8 char min with 1 capital letter and at 1 digit");
+	}
   }
 }
 
@@ -62,69 +114,41 @@ class User
 	private String firstName;
 	private String lastName;
 	private String emailId;
-	private String phoneNumber;
+	private String mobileNumber;
 	private String password;
-	Pattern p;
-	Matcher m;
-	String regex;
-  protected boolean isValid_firstName(String first_Name)
-	{
-    this.firstName=first_Name;
-		regex="^[A-Z]{1}[a-z]+";
-		p = Pattern.compile(regex);
-		if (firstName == null) {
-          return false;
-        }
-		 m= p.matcher(firstName);
-		return m.matches();
-	}
 
-  protected boolean isValid_lastName(String last_Name)
-	{
-    this.lastName=last_Name;
-		regex="^[A-Z]{1}[a-z]+";
-		p = Pattern.compile(regex);
-		if (lastName == null) {
-          return false;
-        }
-		m = p.matcher(lastName);
-		return m.matches();
-	}
+	public String getFirstName(){
+		return firstName;
+	  }
+	public void setFirstName(String firstname) {
+			this.firstName=firstname;
+	  }
 
-	protected boolean isValid_emailId(String emailId)
-	{
-    this.emailId=emailId;
-		 regex="^[A-Z a-z 0-9]+([._+-][0-9 a-z A-Z]+)*@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}([.][a-z A-Z]{2})*$";
-		p = Pattern.compile(regex);
-		if (emailId == null) {
-          return false;
-        }
-		m = p.matcher(emailId);
-		return m.matches();
-	}
+	 public String getLastName(){
+		return lastName;
+	  }
+	  public void setLastName(String lastName) {
+			this.lastName=lastName;
+	 }
 
-	protected boolean isValid_phoneNumber(String phoneNumber)
-	{
-    this.phoneNumber=phoneNumber;
-		 regex="^[1-9]{2}\\s{0,1}[1-9]{1}[0-9]{9}$";
-		p = Pattern.compile(regex);
-		if (phoneNumber== null) {
-          return false;
-        }
-		m = p.matcher(phoneNumber);
-		return m.matches();
-	}
+	 public String getEmailId(){
+		return emailId;
+	  }
+	  public void setEmailId(String emailId) {
+			this.emailId=emailId;;
+	 }
 
-		protected boolean isValid_password(String password)
-	{
-    this.password=password;
-		 regex="^[a-zA-Z]{8,}+$";
-		p = Pattern.compile(regex);
-		if (password== null) {
-          return false;
-        }
-		m = p.matcher(password);
-		return m.matches();
-	}
+	 public String getMobileNumber(){
+		return mobileNumber;
+	  }
+	  public void setMobileNumber(String mobileNumber) {
+			this.mobileNumber=mobileNumber;;
+	 }
 
+	 public String getPassword(){
+		return password;
+	  }
+	  public void setPassword(String password) {
+			this.password=password;;
+	 }
 }
